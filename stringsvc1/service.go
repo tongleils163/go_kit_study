d4e6255562718e7a711ea853d7dee9561a13b2ab
tongleils163 <tongleils@163.com>
2019-06-12 17:05:32 +0800

    stringsvc1

---
+++ stringsvc1/service.go
@@ -0,0 +1,28 @@
+package stringsvc1
+
+import (
+	"errors"
+	"strings"
+)
+
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+type stringService struct {
+}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
